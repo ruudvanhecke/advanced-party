aa01bc42b67efa5f0d8d88088aab2d38ff8998ae
Vanhe <r0808423@student.thomasmore.be>
2021-02-16 10:50:22 +0100

    Introductie

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,12 +1,16 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
+    private final int mySpecialNumber = 729;
+
+    @GetMapping({"/" , "/home"})
+    public String home(Model model) {
+        model.addAttribute("mijnSpeciaalNummer", mySpecialNumber);
         return "home";
     }
 
