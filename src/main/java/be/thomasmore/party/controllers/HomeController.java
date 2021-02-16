fd39f148637ced36b5207916114af40acd1f6fce
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-02-16 12:50:48 +0100

    venuelist

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -3,6 +3,9 @@ package be.thomasmore.party.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
@@ -17,5 +20,14 @@ public class HomeController {
     public String about(Model model){
         model.addAttribute("mijnSpeciaalNummer", mySpecialNumber);
         return "about";
+    }
+    @GetMapping("/venuedetails/{venueName}")
+    public String venueDetails(Model model, @PathVariable String venueName) {
+        model.addAttribute("venuename", venueName);
+        return "venuedetails";
+    }
+    @GetMapping("/venuelist")
+    public String venueList(Model model) {
+        return "venuelist";
     }
 }
