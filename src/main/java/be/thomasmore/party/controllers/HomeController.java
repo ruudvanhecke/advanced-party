2bed644fcde9649c199cfc9a56927010ff4bdc81
Vanhe <r0808423@student.thomasmore.be>
2021-02-17 11:40:54 +0100

    next en prev button.

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 729;
+    private final String[] venueNames = { "De Club", "De Loods", "Zapoi", "Nekkerhal"};
 
     @GetMapping({"/" , "/home"})
     public String home(Model model) {
@@ -23,12 +24,26 @@ public class HomeController {
 
     @GetMapping({"/venuedetails", "/venuedetails/{venueName}"})
     public String venueDetails(Model model, @PathVariable(required = false) String venueName) {
-        model.addAttribute("venuename", venueName==null ? "--no venue specified--" : venueName);
+        model.addAttribute("venuename", venueName);
+        return "venuedetails";
+    }
+
+
+    @GetMapping({"/venuedetailsbyindex", "/venuedetailsbyindex/{venueId}"})
+    public String venueDetailsByIndex(Model model, @PathVariable(required = false) Integer venueId) {
+        if (venueId!=null && venueId>=0 && venueId<venueNames.length)
+        {
+            model.addAttribute("venuename", venueNames[venueId]);
+            model.addAttribute("prev", venueId>0 ? venueId - 1 : venueNames.length - 1);
+            model.addAttribute("next" , venueId< venueNames.length - 1 ? venueId + 1 : 0);
+        }
+        //model.addAttribute("venuename", venueName);
         return "venuedetails";
     }
 
     @GetMapping("/venuelist")
-    public String venuelist(Model model) {
+    public String venueList(Model model) {
+        model.addAttribute("venues", venueNames);
         return "venuelist";
     }
 
