21b54e4b2ec43363bd0b7bc455f54fde1fef1bc0
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-02-17 12:27:23 +0100

    bundel 1 afgewerkt

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -9,27 +9,32 @@ import java.util.ArrayList;
 
 @Controller
 public class HomeController {
-    private final int mySpecialNumber = 729;
+    private final String[] venueNames = { "De Club", "De Loods", "Zapoi", "Nekkerhal" };
 
     @GetMapping({"/","/home"})
     public String home(Model model) {
-        model.addAttribute("mijnSpeciaalNummer", mySpecialNumber);
         return "home";
     }
 
     @GetMapping("/about")
     public String about(Model model){
-        model.addAttribute("mijnSpeciaalNummer", mySpecialNumber);
         return "about";
     }
-    @GetMapping({"/venuedetails", "/venuedetails/{venueName}"})
-    public String venueDetails(Model model, @PathVariable(required = false) String venueName) {
-        model.addAttribute("venuename", venueName==null ? "--no venue specified--" : venueName);
+
+    @GetMapping({"/venuedetails", "/venuedetails/{venueId}"})
+    public String venueDetails(Model model, @PathVariable(required = false) Integer venueId) {
+        if (venueId!=null && venueId>=0 && venueId<venueNames.length) {
+            model.addAttribute("venuename", venueNames[venueId]);
+            model.addAttribute("prev", venueId>0 ? venueId-1 : venueNames.length-1);
+            model.addAttribute("next", venueId<venueNames.length-1 ? venueId+1 : 0);
+        }
         return "venuedetails";
     }
 
     @GetMapping("/venuelist")
     public String venueList(Model model) {
+        model.addAttribute("venues", venueNames);
         return "venuelist";
     }
+
 }
