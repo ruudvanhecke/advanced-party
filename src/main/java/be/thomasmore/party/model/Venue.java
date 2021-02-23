296beaa90fac5d9af380c41d5dc826aeb1b7b65e
Vanhe <r0808423@student.thomasmore.be>
2021-02-23 10:39:40 +0100

    SQL added

--- src/main/java/be/thomasmore/party/model/Venue.java
+++ src/main/java/be/thomasmore/party/model/Venue.java
@@ -1,12 +1,33 @@
 package be.thomasmore.party.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Venue {
+    @Id
+    private int id;
     private String venueName;
     private String linkMoreInfo;
     private int capacity;
+    private boolean foodProvided;
     private boolean indoor;
+    private boolean outdoor;
+    private boolean freeParkingAvailable;
     private String city;
+    private double distanceFromPublicTransportInKm;
 
+
+    public Venue() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public Venue(String venueName, String linkMoreInfo, int capacity, boolean indoor, String city ){
         this.venueName = venueName;
